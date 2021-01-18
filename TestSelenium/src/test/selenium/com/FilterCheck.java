b953dec52450fe992cf8f156d15704ec942bb5fc
rabia <bukharirabia09@gmail.com>
2021-01-18 11:03:06 -0500

    Filter check with use of list

--- TestSelenium/src/test/selenium/com/FilterCheck.java
+++ TestSelenium/src/test/selenium/com/FilterCheck.java
@@ -1,5 +1,10 @@
 package test.selenium.com;
 
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,18 +13,46 @@ import org.openqa.selenium.support.ui.Select;
 public class FilterCheck {
 	SelectTest dr=new SelectTest();
 	WebDriver driver=dr.getDriver();
-	public  void callDriver() {
+	public  void callDriver() throws InterruptedException {
 		driver.findElement(By.cssSelector("input[id$='searchtextbox']")).sendKeys("cars");
 		driver.findElement(By.cssSelector("input#nav-search-submit-button")).click();
 		WebElement sel=driver.findElement(By.id("s-result-sort-select"));
 		Select sel1=new Select(sel);
+		
 		sel1.selectByVisibleText("Price: Low to High");
 		
 	}
 	public void selectBlueRay() throws InterruptedException {
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//div[@id='filters']//li[2]//a")).click();;
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		List<WebElement> pics=driver.findElements(By.xpath("//ul[@aria-labelledby='p_n_format_browse-bin-title']//li//a"));
+		for(int i=0; i<pics.size();i++) {
+			System.out.println(pics.get(i).getText());
+			if(pics.get(i).getText().contains("3D")) {
+			System.out.println(pics.get(i).isDisplayed());
+			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+			pics.get(i).click();
+			System.out.println("I am clicked");
+			break;
+			
+			}
+			
+		}	
 		
+	}
+	
+	public void selectPrice(String pr) {
+		
+		List <WebElement> price=driver.findElements(By.xpath("//a[@class='a-size-base a-link-normal a-text-normal']"));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		for(WebElement i:price) {
+			System.out.println("yes I am here"+i.getText());
+
+			if(i.getText().contains(pr)){
+				
+				System.out.println("my price");
+			}
+		}
+				
 	}
 	public void shut() {
 		driver.close();
@@ -29,7 +62,8 @@ public class FilterCheck {
 		FilterCheck FC=new FilterCheck();
 		FC.callDriver();
 		FC.selectBlueRay();
-	//	FC.shut();
+		FC.selectPrice("15");
+	//FC.shut();
 		
 		
 	}
